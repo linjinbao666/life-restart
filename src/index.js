88fa42c596eb074c240cb0e96b6aef693721af61
linjinbao666 <linjinbao666@gmail.com>
2021-09-07 16:31:48 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import App from '../src/app.js';
 
 window.json = async fileName => await (await fetch(`../data/${fileName}.json`)).json();
 
-// Pssst, I've created a github package - https://github.com/brookesb91/dismissible
 window.hideBanners = (e) => {
     document
         .querySelectorAll(".banner.visible")
